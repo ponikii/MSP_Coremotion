573aee6748150fb97badcee987b2c6cd6fc14561
Harry <harry.1016@icloud.com>
2024-10-31 06:17:51 +0800

    Initial Commit

---
+++ Max-Gyro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Max-Gyro
+//
+//  Created by Allan Shi on 2024/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Max-Gyro/Max_GyroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Max_GyroApp.swift
+//  Max-Gyro
+//
+//  Created by Allan Shi on 2024/10/31.
+//
+
+import SwiftUI
+
+@main
+struct Max_GyroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
